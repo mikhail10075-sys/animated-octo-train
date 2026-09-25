1e895158948364d9e65e45edac54349a24bac862
mikhail10075-sys <mikhail10075@gmail.com>
2026-09-25 12:39:19 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,39 @@
+import pygame
+pygame.init()
+SCREEN_WIDTH,SCREEN_HEIGHT= (500,500)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+title = pygame.display.set_caption(('Image upload...'))
+
+Bg_image = pygame.transform.scale(pygame.image.load('download.jfif').convert(),
+(SCREEN_WIDTH,SCREEN_HEIGHT))
+
+person_image = pygame.transform.scale(pygame.image.load('tiger.png').convert_alpha(),
+(200,200))
+
+person_rect = person_image.get_rect(center = (SCREEN_WIDTH //2, SCREEN_HEIGHT//2-30))
+
+text = pygame.font.Font(None,36).render('Hello World', True, pygame.Color('black'))
+text_rect = text.get_rect(center = (SCREEN_WIDTH //2, SCREEN_HEIGHT//2+110))
+
+def game_loop():
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+           
+           running = False
+        
+        screen.blit(Bg_image, (0,0))
+        screen.blit(person_image, person_rect)
+        screen.blit(text, text_rect)
+
+        pygame.display.flip()
+
+        clock.tick(30)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    game_loop()
